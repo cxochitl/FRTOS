b66b79e8a49c2749e288f5e6ead8d51b361c38a3
Claudia Perez <claudia_xochil@hotmail.com>
2020-10-23 16:09:41 -0500

    Adding functionalities of threads

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -435,18 +435,19 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_2, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13|GPIO_PIN_14, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PA1 PA2 */
-  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2;
+  /*Configure GPIO pins : PC13 PC14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
 }
 
@@ -464,10 +465,17 @@ static void MX_GPIO_Init(void)
 void Thread1(void *argument)
 {
   /* USER CODE BEGIN 5 */
+
+	uint8_t txData[20] = "Hello from thread1\r\n";
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
+		
+		HAL_UART_Transmit(&huart1, txData, 20, 1);
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET ); 
+		osDelay(40);
+		 HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET ); 
+    osDelay(60);
   }
   /* USER CODE END 5 */
 }
@@ -485,7 +493,9 @@ void Thread2(void *argument)
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_14); 
+		
+    osDelay(1000);
   }
   /* USER CODE END Thread2 */
 }
